c8dfbb9ebc7a9b5876c352fcc861b3aec34b3719
oslob99 <jkh991116@naver.com>
2023-03-27 10:38:23 +0900

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println();
+
+        int i = 100;
+
+        double random = Math.random();
+
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
