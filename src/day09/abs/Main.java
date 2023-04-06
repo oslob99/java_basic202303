66a3da7b247b134526fa0ac6b6b2d598b6599d79
oslob99 <jkh991116@naver.com>
2023-04-06 11:24:57 +0900

    4/4 abstract

---
+++ src/day09/abs/Main.java
@@ -0,0 +1,22 @@
+package day09.abs;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pet[] pets = {
+                new Dog("해피", "푸들",2),
+                new Dog("초코", "시츄",3),
+                new Dog("최강", "불독",4),
+                new Cat("꾸꾸까까", "러시안블루",2)
+        };
+
+//        new Pet("그냥","그냥종", 1);
+
+        for (Pet pet : pets) {
+            pet.takeNap();
+        }
+
+    }
+
+}
