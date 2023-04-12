271eeb03950ef9d16249e1f76a5382e09124302c
oslob99 <jkh991116@naver.com>
2023-04-13 01:31:38 +0900

    4/13 프로젝트 업로드

---
+++ src/sportsFacArea/SportAreaRepository.java
@@ -0,0 +1,46 @@
+package sportsFacArea;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SportAreaRepository {
+
+    static List<String> areaList;
+    static SportsFacList sportList;
+    private int areaListIndex;
+
+
+
+    static {
+        areaList = List.of("강동구", "강서구","강남구","강북구");
+        sportList = new SportsFacList();
+    }
+
+
+
+    public SportAreaRepository() {
+    }
+
+    public SportAreaRepository(int areaListIndex) {
+        this.areaListIndex = areaListIndex;
+    }
+
+    public int getAreaListIndex() {
+        return areaListIndex;
+    }
+
+    public void setAreaListIndex(int areaListIndex) {
+        this.areaListIndex = areaListIndex;
+    }
+
+    public void showArea(){ // 지역구 리스트 출력하기
+        for (int i = 0; i < areaList.size(); i++) {
+            System.out.printf("%d. %s\n",i+1,areaList.get(i));
+        }
+    }
+    public List<String> callListArea(){
+        return areaList;
+    }
+
+
+}
